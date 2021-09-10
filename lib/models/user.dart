c42eaffe82dc34a13d9acdc1ba231702efb0a2d6
Daniel Martin <damakur@gmail.com>
2021-09-10 23:18:47 +0200

    Frontend terminado

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  String userName;
+  String email;
+  String profilePicture;
+  String uid;
+
+  UserModel({
+    this.userName,
+    this.email,
+    this.profilePicture,
+    this.uid,
+  });
+}
---
+++ lib/screens/favs/my_ads_screen.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:chat_app/widgets/home/ad_item.dart';
+
+class MyAds extends StatefulWidget {
+  @override
+  _MyAdsState createState() => _MyAdsState();
+}
+
+class _MyAdsState extends State<MyAds> {
+  final uid = FirebaseAuth.instance.currentUser.uid;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('products')
+          .where('uid', isEqualTo: uid)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        var documents = snapshot.data.documents;
+        if (snapshot.data.documents.length == 0) {
+          return Center(
+            child: Text('You haven\'t posted any ads'),
+          );
+        }
+        return Padding(
+          padding: EdgeInsets.all(10),
+          child: GridView.builder(
+            itemCount: documents.length,
+            itemBuilder: (context, i) {
+              return AdItem(
+                documents[i],
+                documents[i]['uid'] == uid,
+                uid,
+              );
+            },
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 3 / 2,
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
